1f06206c9d167774513cd8da4c04b13ffcd1062e
M4Y33D <55606847+M4Y33D@users.noreply.github.com>
2022-04-10 20:00:28 +0100

    Add files via upload
    added java buttons for discount on html

---
+++ CTEC3905_Front-End_Web_Development-V7/script/script.js
@@ -0,0 +1,12 @@
+/* the function below will find the element by it id and output a message*/
+function discount()
+{
+    document.getElementById("promo").innerHTML = "dairyliouse for 15% on all online and phone orders"
+}
+
+function Para_third_year()
+{
+    document.getElementById("third year").innerHTML = "hello third years"
+}
+
+window.alert("Welcome to the lands of deliouse treats") 
